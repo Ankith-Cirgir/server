c2993159a6b2feb57a00d459ebdddd468ecf6f22
Ankith_Cirgir <51317745+Ankith-Cirgir@users.noreply.github.com>
2021-02-17 23:38:26 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,6 @@ PORT = 8000
 
 handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("192.168.2.7", PORT), handler) as httpd:
-    print("Server started at localhost:" + str(PORT))
+with socketserver.TCPServer(("192.168.0.8", PORT), handler) as httpd:
+    print("Server started at 192.168.0.8:" + str(PORT))
     httpd.serve_forever()
